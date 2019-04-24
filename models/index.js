2d951a278e75ac8c65c070ae47ab671329d13942
Ashell Fernando <ashellz2010@gmail.com>
2019-04-24 19:03:47 -0400

    fixed sending pass issue

--- models/index.js
+++ models/index.js
@@ -1,28 +1,30 @@
-"use strict";
+'use strict';
 
-var fs = require("fs");
-var path = require("path");
-var Sequelize = require("sequelize");
+var fs = require('fs');
+var path = require('path');
+var Sequelize = require('sequelize');
 var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/config.json")[env];
+var env = process.env.NODE_ENV || 'development';
+var config = require(__dirname + '/../config/config.json')[env];
+
 var db = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable], {
+    operatorsAliases: 'Op'
+  });
+} else if (config.use_env_local) {
+  var sequelize = new Sequelize(process.env[config.use_env_local], {
+    operatorsAliases: 'Op'
+  });
 } else {
-  var sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
+  return;
 }
 
 fs.readdirSync(__dirname)
   .filter(function(file) {
     return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
     );
   })
   .forEach(function(file) {
