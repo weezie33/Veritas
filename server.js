e9fc5a4a47ce8450abb560a87c844c3aa93058a9
Ashell Fernando <ashellz2010@gmail.com>
2019-04-24 19:44:18 -0400

    changed console log of server listening

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
-const express = require("express");
-const exphbs = require("express-handlebars");
-const bodyParser = require('body-parser')
-const db = require("./models");
+require('dotenv').config();
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const db = require('./models');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,42 +10,38 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Handlebars
 app.engine(
-  "handlebars",
+  'handlebars',
   exphbs({
-    defaultLayout: "main"
+    defaultLayout: 'main'
   })
 );
-app.set("view engine", "handlebars");
+app.set('view engine', 'handlebars');
 
-if(app.get('env')==='development'){
-  const logger = require('morgan')
+if (app.get('env') === 'development') {
+  const logger = require('morgan');
   app.use(logger('dev'));
 }
 
 // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
+if (process.env.NODE_ENV === 'test') {
   syncOptions.force = true;
 }
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
+    console.log('Listening on http://localhost:%s/', PORT, PORT);
   });
 });
 
