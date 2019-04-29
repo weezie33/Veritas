dfb75ab1d671b244f35258f5f239d465e1774413
Ashell Fernando <ashellz2010@gmail.com>
2019-04-29 17:17:24 -0400

    webhose is working at the moment (not persistant)

--- api/webhoseio.js
+++ api/webhoseio.js
@@ -3,30 +3,36 @@ module.exports = (req, res) => {
   const app = require('../server');
   const db = require('../models');
   const webhoseio = require('webhoseio');
+  const moment = require('moment');
 
   const client = webhoseio.config({
     token: process.env.WEBHOSEIO_TOKEN
   });
 
+  const STORES =
+    '(site:walmart.com OR site:kroger.com OR site:amazon.com OR site:costco.com OR site:homedepot.com OR site:walgreens.com OR site:cvs.com OR site:target.com OR site:lowes.com OR site:apple.com OR site:bestbuy.com OR site:macys.com OR site:riteaid.com OR site:starbucks.com OR site:rossstores.com)';
+
+  let unixTime = () => {
+    let days = moment()
+      .subtract(30, 'days')
+      .calendar();
+    let date = new Date(days);
+    return date.valueOf();
+  };
   const query_params = {
-    q:
-      'item.title:' +
-      req.params.item_title +
-      ' ' +
-      'site:amazon.com' +
-      ' ' +
-      'rating:>0',
-    ts: '1553965699215',
+    q: 'item.title:' + req.params.item_title + ' ' + STORES + ' ' + 'rating:>0',
+    ts: unixTime(),
     sort: 'rating'
   };
   client
     .query('reviewFilter', query_params)
     .then(output => {
-      console.log(output.reviews); // Print the text of the first review
-      console.log(output['reviews']['published']); // Print the text of the first review publication date
-      console.log(output);
+      console.log(output.reviews);
+      // console.log(output['reviews']['published']); // Print the text of the first review publication date
+      // console.log(output);
       // next();
-      res.render('webhose', { output });
+      let reviews = output.reviews;
+      res.render('webhose', { reviews });
     })
     .catch(err => {
       if (err) throw err;
