3be584a6417165db092f82a48561ae07254126ca
Ashell Fernando <ashellz2010@gmail.com>
2019-04-29 08:44:17 -0400

    webhose api

---
+++ api/webhoseio.js
@@ -0,0 +1,23 @@
+require('../node_modules/dotenv').config();
+module.exports = (req, res, next) => {
+  const db = require('../models');
+  const webhoseio = require('../node_modules/webhoseio');
+
+  const client = webhoseio.config({
+    token: process.env.WEBHOSEIO_TOKEN
+  });
+
+  const query_params = {
+    q: `item.title:${req.body.search_term} site:${
+      req.body.site_name
+    } rating:>0`,
+    ts: '1553535891983',
+    sort: 'rating'
+  };
+  client.query('reviewFilter', query_params).then(output => {
+    console.log(output['reviews']['text']); // Print the text of the first review
+    console.log(output['reviews']['published']); // Print the text of the first review publication date
+    res.send(output['reviews']['text']);
+  });
+  next();
+};
