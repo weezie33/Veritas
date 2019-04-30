6a7cdade9adb9280fdb9122bcf337532e2afad09
Ashell Fernando <ashellz2010@gmail.com>
2019-04-30 18:24:30 -0400

    added validations to check if params are there

--- api/webhoseio.js
+++ api/webhoseio.js
@@ -18,74 +18,82 @@ module.exports = (req, res) => {
     let date = new Date(days);
     return date.valueOf();
   };
-  const query_params = {
-    q:
-      'item.title:' +
-      req.params.item_title +
-      '$' +
-      ' ' +
-      STORES +
-      ' ' +
-      'rating:>0',
-    ts: unixTime(),
-    sort: 'rating'
-  };
+  if (req.params.item_title || req.body.item_title) {
+    const query_params = {
+      q:
+        'item.title:' +
+        (req.params.item_title || req.body.item_title) +
+        '$' +
+        ' ' +
+        STORES +
+        ' ' +
+        'rating:>0',
+      ts: unixTime(),
+      sort: 'rating'
+    };
 
-  client
-    .query('reviewFilter', query_params)
-    .then(output => {
-      // console.log(output.reviews);
-      for (const key in output.reviews) {
-        console.log(
-          `${key + 1} = ${JSON.stringify(output.reviews[key], null, 2)}`
-        );
-        let currentKey = output.reviews[key];
-        db.reviews
-          .bulkCreate([
-            {
-              uuid: currentKey.uuid,
-              url: currentKey.url,
-              author: currentKey.author,
-              published: currentKey.published,
-              title: currentKey.title,
-              text: currentKey.text,
-              language: currentKey.language,
-              rating: currentKey.rating,
-              crawled: currentKey.crawled
-            }
-          ])
-          .then(dbres => {
-            console.log(`Successfully logged into ReviewsTable`);
-          });
-        let itemKey = currentKey.item;
+    client
+      .query('reviewFilter', query_params)
+      .then(output => {
+        // console.log(output.reviews);
+        for (const key in output.reviews) {
+          console.log(
+            `${key + 1} = ${JSON.stringify(output.reviews[key], null, 2)}`
+          );
+          let currentKey = output.reviews[key];
+          db.reviews
+            .bulkCreate([
+              {
+                uuid: currentKey.uuid,
+                url: currentKey.url,
+                author: currentKey.author,
+                published: currentKey.published,
+                title: currentKey.title,
+                text: currentKey.text,
+                language: currentKey.language,
+                rating: currentKey.rating,
+                crawled: currentKey.crawled
+              }
+            ])
+            .then(dbres => {
+              console.log(
+                `${new Date()} Successfully logged into ReviewsTable`
+              );
+            });
+          let itemKey = currentKey.item;
 
-        db.products
-          .bulkCreate([
-            {
-              uuid: itemKey.uuid,
-              url: itemKey.url,
-              site: itemKey.site,
-              site_section: itemKey.site_section,
-              title: itemKey.title,
-              published: itemKey.published,
-              reviews_count: itemKey.reviews_count,
-              reviewers_count: itemKey.reviewers_count,
-              country: itemKey.country,
-              spam_score: itemKey.spam_score,
-              main_image: itemKey.main_image
-            }
-          ])
-          .then(dbres => {
-            console.log(`Successfully logged into ProductsTable`);
-          });
-      }
-      // console.log(output['reviews']['published']); // Print the text of the first review publication date
-      // console.log(output);
-      // next();
-      let reviews = output.reviews;
-      res.render('webhose', { reviews });
-    })
-    .catch(err => {
-      if (err) throw err;
-    });
+          db.products
+            .bulkCreate([
+              {
+                uuid: itemKey.uuid,
+                url: itemKey.url,
+                site: itemKey.site,
+                site_section: itemKey.site_section,
+                title: itemKey.title,
+                published: itemKey.published,
+                reviews_count: itemKey.reviews_count,
+                reviewers_count: itemKey.reviewers_count,
+                country: itemKey.country,
+                spam_score: itemKey.spam_score,
+                main_image: itemKey.main_image
+              }
+            ])
+            .then(dbres => {
+              console.log(
+                `${new Date()} Successfully logged into ProductsTable`
+              );
+            });
+        }
+
+        let reviews = output.reviews;
+        res.render('webhose', { reviews });
+        return next();
+      })
+      .catch(err => {
+        if (err) throw err;
+      });
+  } else {
+    let qErr = `Could not find the payload, try again!`;
+    res.redirect('/404?qErr=' + qErr);
+  }
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,28 +5,40 @@ const yelp = require('../api/yelp');
 module.exports = function(app) {
   db.products.hasMany(db.reviews, { foreignKey: 'id' });
   db.reviews.belongsTo(db.products, { foreignKey: 'id' });
-  // Load index page
-  app.get('/reviews', function(req, res) {
-    db.reviews.findAll({}).then(function(result) {
-      res.render('index', {
-        result
-      });
-    });
+  // Loads the reviews page, should be in apiRoutes
+  // app.get('/reviews', function(req, res) {
+  //   db.reviews.findAll({}).then(function(result) {
+  //     res.render('index', {
+  //       result
+  //     });
+  //   });
+  // });
+
+  //sets up query variables
+  app.get('/404', function(req, res) {
+    let qErr = req.query.qErr;
+    res.render('404', { qErr });
   });
-  app.get('/reviews/:id', function(req, res) {
-    db.reviews
-      .findAll({
-        where: {
-          id: req.params.id
-        }
-      })
-      .then(function(result) {
-        console.log(result);
-        res.render('dbtest', {
-          result
-        });
-      });
+
+  //Loads the index.handlebars on homepage
+  app.get('/', (req, res) => {
+    res.render('index');
   });
+
+  // app.get('/api/reviews/:id', function(req, res) {
+  //   db.reviews
+  //     .findAll({
+  //       where: {
+  //         id: req.params.id
+  //       }
+  //     })
+  //     .then(function(result) {
+  //       // res.render('dbtest', {
+  //       //   result
+  //       // });
+  //       res.json(result);
+  //     });
+  // });
 
   app.get('/dbtest', function(req, res) {
     if (req.params.search === 'favicon.ico') {
@@ -43,10 +55,6 @@ module.exports = function(app) {
 
   app.get('/', function(req, res) {
     res.render('index');
-  });
-
-  app.get('/search/:item_title', function(req, res) {
-    webhoseio(req, res);
   });
 
   app.get('/yelp/:name/:location', (req, res) => {
